76a3f63694fbb8f84b49825d0c482216b4cceaa8
Kateve52911 <kateve52911@stud.noroff.no>
2026-07-24 13:01:50 +0200

    made error handling for fetch function more spesific (added more 400s and 500s errors and one that covers the rest) and moved the fetching of data into an if/else statement and moved the if/else statement into a try/catch block

--- src/services/api/products.ts
+++ src/services/api/products.ts
@@ -1,26 +1,38 @@
 import {BASE_URL, SHOP_URL} from "./config.ts";
+import {APIError} from "./apiError.ts";
+import type {Product} from "../../schemas/product.ts";
 
-export async function fetchProducts() {
+export async function fetchProducts():Promise<Product[]> {
     const URL = `${BASE_URL}${SHOP_URL}`;
     console.log(`Fetching ${URL}`);
+    let response
     try {
-        const response = await fetch(URL)
-
+        response = await fetch(URL)
         if(!response.ok) {
-            throw new Error(
-                `An error occurred: ${response.status} ${response.statusText}`,
-            );
+            if (response.status === 404) {
+                throw new APIError(`Could not find ${URL}`, response.status);
+            } else if (response.status === 401 || response.status === 403) {
+                throw new APIError(`Unauthorized or fobidden`, response.status);
+            }else if (response.status === 400) {
+                throw new APIError(`Bad request to the API`, response.status);
+            } else if (response.status >= 500 && response.status <= 505) {
+                throw new APIError(`Request was valid but server could not fullfill the request`, response.status);
+            } else {
+                throw new APIError(`Could not fetch the data`, response.status);
+            }
+        } else {
+            const { data } = await response.json();
+            console.log(data);
+            return data;
+        }
+    } catch (error) {
+        if(error instanceof APIError) {
+            throw error;
+        } else {
+            throw new APIError(`Could not load the data: ${response?.status}`, undefined)
         }
 
-        const data = await response.json();
-        console.log(data);
-        return data;
-
-    } catch (error) {
-        throw new Error('Failed to fetch products:', error);
     }
-
-
 }
 
 await fetchProducts()
